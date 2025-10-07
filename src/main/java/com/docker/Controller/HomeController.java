148fc995c92074f60b606cbd09e9d3017ffbf9dc
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2025-10-07 23:27:25 +0530

    firt comit

---
+++ src/main/java/com/docker/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.docker.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String welcome() {
+        return "hello all...";
+    }
+
+}
